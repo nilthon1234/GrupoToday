aedc9adb6ec8303807c1c26b811134a815df5f5b
deanAnder <dean2anderhm@gmail.com>
2024-07-10 00:26:43 -0500

    agregando paginas carrito index zapatillas  y listando las categorias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'node:path';
+import { IndexComponent } from './index/index.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent},
+  { path: 'zapatillas', component: ZapatillasComponent},
+  { path: 'carrito', component: CarritoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule} from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import { CarritoComponent } from './carrito/carrito.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CarritoComponent,
+    ZapatillasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    HttpClientModule,
+    
+
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [
     provideClientHydration()
